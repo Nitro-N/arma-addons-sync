606592538745d846b123b038378985bafb8205a3
Nitro-N <nitron.ru@gmail.com>
2018-06-07 09:36:50 +0700

    Update project settings and packages for better debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,47 @@
-const path = require('path');
-const BannerPlugin = require('banner-webpack-plugin');
+module.exports = (env, argv) => {
+    const path = require('path');
+    const BannerPlugin = require('banner-webpack-plugin');
 
-module.exports = {
-    entry: {
-        'index': './src/index.ts',
-        'cli': './src/cli.ts'
-    },
-    target: 'node',
-    node: {
-        __dirname: false,
-        __filename: false
-    },
-    module: {
-        rules: [
-            {
+    const isDev = argv.mode === 'development';
+    const isProd = !isDev;
+
+    const config = {
+        entry: {
+            'index': './src/index.ts',
+            'cli': './src/cli.ts'
+        },
+        target: 'node',
+        node: {
+            __dirname: false,
+            __filename: false
+        },
+        module: {
+            rules: [{
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            }]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+        },
+        plugins: []
+    };
+
+    if (isProd) {
+        config.plugins.push(new BannerPlugin({
+            chunks: {
+                cli: {
+                    beforeContent: '#!/usr/bin/env node\n'
+                }
             }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    devtool: 'cheap-source-map',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
-    },
-    plugins: [
-        new BannerPlugin({chunks: {
-            cli: {
-                beforeContent: '#!/usr/bin/env node\n'
-            }
-        }})
-    ]
-};
+        }))
+    }
+
+    return config;
+}
